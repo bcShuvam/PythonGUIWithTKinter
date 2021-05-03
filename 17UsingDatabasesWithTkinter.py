b28da3b5415eca21590e911d329c531a9c23abf6
bcShuvam <bcshuvam@gmail.com>
2021-05-03 10:50:16 +0545

    D5 Using SQL lite3 database with tkinter

---
+++ 17UsingDatabasesWithTkinter.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import sqlite3
+
+root = Tk()
+root.title("Using SQLlite3 Databases")
+root.iconbitmap("images/tkinter.ico")
+root.geometry("400x400")
+
+# CREATE a database or connect to existing one
+
+conn = sqlite3.connect('address_book.db')
+
+c = conn.cursor()
+
+# Create a table
+c.execute("""CREATE TABLE address (
+    first_name text,
+    last_name text,
+    address text,
+    city text,
+    state text,
+    zipcode integer
+    )""")
+
+# Commit Changes
+conn.commit()
+
+# Close Connection
+conn.close()
+
+root.mainloop()
