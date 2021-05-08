7fc635995f8de6e8d40e65ad83597e587504806c
bcShuvam <bcshuvam@gmail.com>
2021-05-08 21:13:29 +0545

    D9 Adding Zipcode search bar to Weather App

--- 20WeatherApp.py
+++ 20WeatherApp.py
@@ -1,28 +1,54 @@
 from tkinter import *
-from PIL import ImageTk,Image
-import sqlite3
 import requests
 import json
 
 root = Tk()
 root.title("Using SQLlite3 Databases")
 root.iconbitmap("images/tkinter.ico")
-root.geometry("450x40")
+root.geometry("600x100")
 root.configure(background="#2d3436")
 
 # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=10&API_KEY=9049FF59-56C4-4AE1-BD9F-DECCAFFFA37F
 
 
-try:
-    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=10&API_KEY=9049FF59-56C4-4AE1-BD9F-DECCAFFFA37F")
-    api = json.loads(api_request.content)
-    city = api[0]['ReportingArea']
-    quality = api[0]['AQI']
-    category = api[0]['Category']['Name']
-except EXCEPTION as e:
-    api = "Error 404"
+def ziplookup():
+    # print(zip.get())
 
-myLabel = Label(root,text=f"{city} Air Quality {quality} {category}",font=("Arial",20),bg="#27ae60",fg="#ffffff")
-myLabel.pack()
+    weather_color = ''
+    api = ''
+    city = ''
+    quality = ''
+    category = ''
+
+    try:
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+zip.get()+"&distance=10&API_KEY=9049FF59-56C4-4AE1-BD9F-DECCAFFFA37F")
+        api = json.loads(api_request.content)
+        city = api[0]['ReportingArea']
+        quality = api[0]['AQI']
+        category = api[0]['Category']['Name']
+
+        if category == "Good":
+            weather_color = "#0C0"
+        elif category == "Moderate":
+            weather_color = "#FFFF00"
+        elif category == "Unhealthy for sensitive Groups":
+            weather_color = "#ff9900"
+        elif category == "Unhealthy":
+            weather_color = "#FF0000"
+        elif category == "Very Unhealthy":
+            weather_color = "#990066"
+        elif category == "Hazardous":
+            weather_color = "#660000"
+
+        myLabel = Label(root, text=f"{city} Air Quality {quality} {category}", font=("Arial", 20), bg=weather_color,fg="#ffffff")
+        myLabel.grid(row=1,column=0,columnspan=2)
+    except EXCEPTION as e:
+        api = "Error 404"
+
+zip = Entry(root)
+zip.grid(row=0,column=0,stick=W+E+N+S)
+
+zipButton = Button(root,text="Lookup Zipcode",command=ziplookup)
+zipButton.grid(row=0,column=1,stick=W+E+N+S)
 
 root.mainloop()
